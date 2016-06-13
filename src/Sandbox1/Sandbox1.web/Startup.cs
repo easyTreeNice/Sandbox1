5c117abdf2610b19a6dab648e5040ab9d10b4351
Lee Doran-Constant <lee@doran-constant.co.uk>
2016-06-13 14:43:37 +0100

    Initial commit.

---
+++ src/Sandbox1/Sandbox1.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sandbox1.web.Startup))]
+namespace Sandbox1.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
